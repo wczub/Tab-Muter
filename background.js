4ad3d0b883e25322d6bed6359ce709ba3a374468
wczub <wczub@users.noreply.github.com>
2019-01-15 21:02:46 -0800

    Initial Commit
    Initial Commit

---
+++ background.js
@@ -0,0 +1,20 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		var tab = tabs[0]
+		if (tab.mutedInfo.muted == false){
+			chrome.tabs.update(tab.id, {muted: true}), null;
+			chrome.tabs.sendMessage(tab.id, {"title": "[M]"}, function (response){
+				console.log(response);
+			});
+		}
+		else {
+			chrome.tabs.update(tab.id, {muted: false}, null);
+			chrome.tabs.sendMessage(tab.id, {"title": ""}, function (response){
+				console.log(response);
+			});
+		}
+		console.log(tab.mutedInfo);
+		
+	});
+    
+})
